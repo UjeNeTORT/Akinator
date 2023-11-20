52e65ecfc65ab55c4da831d85b542b273f9414e7
UjeNeTORT <tikhonovty@gmail.com>
2023-11-20 09:19:59 +0300

    html writer + non critial changes

--- tree_dump/tree_dump.cpp
+++ tree_dump/tree_dump.cpp
@@ -2,52 +2,108 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
+
 
 #include "tree_dump.h"
 
-int TreeDump(const char * fname, Tree * tree)
+int TreeDump (const char * fname, Tree * tree)
 {
-    assert(fname);
+    assert (fname);
 
-    char * command = (char *) calloc(COMMAND_BUF_SIZE, sizeof(char));
+    char * command = (char *) calloc (COMMAND_BUF_SIZE, sizeof(char));
 
     int dump_id = rand();
 
-    sprintf(command, "dot -Tpng %s%s -o %sgraph_dump_%d.png", DOT_FILE_PATH, fname, GRAPH_PNGS_PATH, dump_id);
-    system(command);
+    DotTreePrint (fname, tree);
+
+    sprintf (command, "dot -Tpng %s%s -o %sgraph_dump_%d.png", DOT_FILE_PATH, fname, GRAPH_PNGS_PATH, dump_id);
+    system (command);
+
+    free(command);
+
+    WriteHTML("dump.html", dump_id, 0);
 
     return 0;
 }
 
-char * CreateTreeGraph (const char * dot_fname, Tree * tree)
+int DotTreePrint (const char * dot_fname, Tree * tree)
 {
-    assert(dot_fname);
+    assert (dot_fname);
 
-    char * dot_code = (char *) calloc(DOT_CODE_BUF_SIZE, sizeof(char));
+    chdir("tree_dump/dumps/dot_files");
 
+    FILE * dot_file = fopen (dot_fname, "wb");
 
+    fprintf (dot_file, "digraph TREE {\n");
 
-    return dot_code;
+    DotSubtreePrint (dot_file, tree->root);
+
+    fprintf (dot_file, "}\n");
+
+    fclose (dot_file);
+
+    chdir("../../../");
+
+    return 0;
 }
 
 int WriteHTML (const char * HTML_fname, int dump_id, size_t err_vec)
 {
-    assert(HTML_fname);
+    assert (HTML_fname);
 
-    time_t t = time(NULL);
+    time_t t = time (NULL);
 
-    tm * loc_time = localtime(&t);
+    tm * loc_time = localtime (&t);
 
-    FILE * HTML_file = fopen(HTML_fname, "ab");
+    chdir("tree_dump/dumps/dumps");
 
-    fprintf(HTML_file, "<p style=\"font-family:monospace; font-size: 20px\">[%s]\nTREE DUMP of \"\%s\" called from \%s (\%d) from \%s</p>", asctime(loc_time));
+    FILE * HTML_file = fopen (HTML_fname, "ab");
 
-    fprintf(HTML_file, "<img src=\"./%sgraph_dump_%d.png\">\n", GRAPH_PNGS_PATH, dump_id);
+    fprintf (HTML_file, "<p style=\"font-family:monospace; font-size: 20px\">[%s]\nTREE DUMP of \"s\" called from s (d) from s</p>", asctime(loc_time));
 
-    fprintf(HTML_file, "<hr>\n", dump_id);
-    fprintf(HTML_file, "<hr>\n", dump_id);
+    fprintf (HTML_file, "<img src=\"./%sgraph_dump_%d.png\">\n", GRAPH_PNGS_PATH, dump_id);
 
-    fclose(HTML_file);
+    fprintf (HTML_file, "<hr>\n");
+    fprintf (HTML_file, "<hr>\n");
+
+    fclose (HTML_file);
+
+    chdir("../../../");
 
     return 0;
+}
+
+int DotSubtreePrint (FILE * stream, TreeNode * node)
+{
+    assert (stream);
+
+    if (!node)
+    {
+
+        return 0;
+    }
+
+    int node_id = rand ();
+
+    // todo function
+    const char * shape = "";
+    if (node->left || node->right)
+    {
+        shape = "diamond";
+    }
+    else
+    {
+        shape = "box";
+    }
+
+    fprintf (stream, "   node_%d [shape = %s, label = \"%s\"]\n", node_id, shape, node->data);
+
+    if (int left_subtree_id = DotSubtreePrint (stream, node->left))
+        fprintf (stream, "   node_%d -> node_%d\n", node_id, left_subtree_id);
+
+    if (int right_subtree_id = DotSubtreePrint (stream, node->right))
+        fprintf (stream, "   node_%d -> node_%d\n", node_id, right_subtree_id);
+
+    return node_id;
 }
